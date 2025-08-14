0a84f8b84530591f123200ad570fcc3ada1f6e08
KenanBarri <Kenan.K@barri.sa>
2025-08-14 06:54:06 +0300

    Assingment is Done (:

--- website-builder/src/components/SectionLibrary.tsx
+++ website-builder/src/components/SectionLibrary.tsx
@@ -51,7 +51,7 @@ export default function SectionLibrary() {
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
                 viewBox="0 0 24 24"
-                stroke-width="1.5"
+                strokeWidth="1.5"
                 stroke="currentColor"
                 className="size-8 drop-shadow-md stroke-orange-400 stroke-1 hover:stroke-orange-600 hover:cursor-pointer"
                 onClick={() => setIsSectionLibraryOpened(false)}
@@ -99,14 +99,14 @@ export default function SectionLibrary() {
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
           viewBox="0 0 24 24"
-          stroke-width="1.5"
+          strokeWidth="1.5"
           stroke="currentColor"
           className="size-10 mt-4 mr-4 drop-shadow-md stroke-orange-400 stroke-2 hover:stroke-orange-600 hover:cursor-pointer"
           onClick={() => setIsSectionLibraryOpened(true)}
         >
           <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
+            strokeLinecap="round"
+            strokeLinejoin="round"
             d="M15.75 9V5.25A2.25 2.25 0 0 0 13.5 3h-6a2.25 2.25 0 0 0-2.25 2.25v13.5A2.25 2.25 0 0 0 7.5 21h6a2.25 2.25 0 0 0 2.25-2.25V15m3 0 3-3m0 0-3-3m3 3H9"
           />
         </svg>
